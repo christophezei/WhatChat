b0e8dafe5a193ef1caf9539f72ec1aa05eec45f3
christophe <christophezeinaty@hotmail.com>
2020-03-26 13:38:50 +0100

    -added new jar files.

--- src/com/christophe/rmi/client/ClientDriver.java
+++ src/com/christophe/rmi/client/ClientDriver.java
@@ -12,7 +12,7 @@ import com.christophe.rmi.server.IServer;
 public class ClientDriver {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
-		String serverURL = "rmi://127.0.0.1/RMIChatServer";
+		String serverURL = "rmi://127.0.0.1:5000/RMIChatServer";
 		IServer server = (IServer) Naming.lookup(serverURL);
 		String userName = args[0];
 		new Thread(new Client(userName, server)).start();
