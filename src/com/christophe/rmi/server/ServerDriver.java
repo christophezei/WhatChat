7a94fb29a47579b72fa347428b22b2697c26f0f4
christophe <christophezeinaty@hotmail.com>
2020-03-26 12:54:54 +0100

    -added creation of registry from code.

--- src/com/christophe/rmi/server/ServerDriver.java
+++ src/com/christophe/rmi/server/ServerDriver.java
@@ -3,13 +3,14 @@ package com.christophe.rmi.server;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-
-import javax.swing.JFrame;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ServerDriver {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
-		Naming.rebind("RMIChatServer", new Server());
+		Registry registry = LocateRegistry.createRegistry(5000);
+		registry.rebind("RMIChatServer", new Server());
 		System.out.println("Server Ready!");
 	}
 
